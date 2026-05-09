b672a28561eaef3e056e25bb7af61578041ece51
irsergeev <irsergeev.ewp@gmail.com>
2026-05-09 21:27:34 +0400

    Initial, added emtpy projects and base solution structure

---
+++ src/PdfProcessing.BS.PdfReaderRabbitMq/Worker.cs
@@ -0,0 +1,9 @@
+namespace PdfProcessing.BS.PdfReaderRabbitMq
+{
+    public class Worker : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+        }
+    }
+}
