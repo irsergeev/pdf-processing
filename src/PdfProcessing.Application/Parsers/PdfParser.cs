1b3e1c564b3671265784498f41fac3655d6c09f9
irsergeev <irsergeev.ewp@gmail.com>
2026-05-10 16:50:13 +0400

    added the library for the pdf parsing, implemented parser and handler

---
+++ src/PdfProcessing.Application/Parsers/PdfParser.cs
@@ -0,0 +1,41 @@
+﻿using PdfProcessing.Application.Interfaces;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.WordExtractor;
+
+namespace PdfProcessing.Application.Parsers;
+
+public class PdfParser : IPdfParser
+{
+    public Task<string> GetContentString(byte[] pdfAsBytesArray)
+    {
+        var emptyStringResult = Task.FromResult(string.Empty);
+
+        if (pdfAsBytesArray == null || pdfAsBytesArray.Length == 0)
+        {
+            return emptyStringResult;
+        }
+
+        try
+        {
+            using var pdfDocument = PdfDocument.Open(pdfAsBytesArray);
+
+            foreach (Page page in pdfDocument.GetPages())
+            {
+                string text = ContentOrderTextExtractor.GetText(page);
+                IEnumerable<Word> words = page.GetWords(NearestNeighbourWordExtractor.Instance);
+
+                var stringResult = string.Join(" ", words.Select(c => c.Text));
+                return Task.FromResult(stringResult);
+            }
+
+            return emptyStringResult;
+        }
+        catch (Exception ex)
+        {
+            // to so something
+            return emptyStringResult;
+        }
+    }
+}
--- src/PdfProcessing.Application/Services/RabbitMQ/Handlers/CreatePdfEventHandler.cs
+++ src/PdfProcessing.Application/Services/RabbitMQ/Handlers/CreatePdfEventHandler.cs
@@ -1,14 +1,37 @@
 ﻿using PdfProcessing.Application.Interfaces;
 using PdfProcessing.Infrastructure.Integration.Contracts.Events;
+using PdfProcessing.Infrastructure.Persistence.Enums;
 
 namespace PdfProcessing.Application.Services.RabbitMQ.Handlers;
 
-public class CreatePdfEventHandler (IPdfService pdfService) : ICreatePdfEventHandler
+public class CreatePdfEventHandler (
+    IPdfService pdfService,
+    IPdfParser pdfParser
+    ) : ICreatePdfEventHandler
 {
     private readonly IPdfService _pdfService = pdfService;
+    private readonly IPdfParser _pdfParser = pdfParser;
 
     public async Task HandleAsync(CreatePdfEvent eventData, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (eventData.DocumentContent == null || eventData.DocumentContent.Length == 0)
+        {
+            return;
+        }
+
+        try
+        {
+            var newPdfId = await _pdfService.CreateAsync();
+            await _pdfService.SetUploadingStatus(newPdfId, UploadingStatusEnum.GettingText);
+
+            var pdfContentAsString = await _pdfParser.GetContentString(eventData.DocumentContent);
+
+            await _pdfService.UpdateStringContent(newPdfId, pdfContentAsString);
+            await _pdfService.SetUploadingStatus(newPdfId, UploadingStatusEnum.Uploaded);
+        }
+        catch(Exception ex)
+        {
+            // to do something
+        }
     }
 }
